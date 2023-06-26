aef89a0af7ea8da24b116bd2cc724797d116e9dd
ANISH JAIN <108573897+anishjain1201@users.noreply.github.com>
2023-06-26 14:33:57 +0530

    Add files via upload

---
+++ decode.py
@@ -0,0 +1,65 @@
+def recover(file_name):
+
+    from reedsolo import RSCodec
+    from pathlib import Path
+    # from encode import size_of_file
+
+    file_name="input"
+
+    fli = "./media/"+file_name+".txt"
+
+    fle="./media/"+file_name+"_encode.bin"
+
+    f = open(fle, 'rb')
+
+    parity=f.read()
+
+    f.close()
+
+    fle_s ="./media/"+file_name+"_original_len.txt"
+    f = open(fle_s, 'r')
+    size = f.read()
+
+    size_of_file = int(size)
+
+    f.close()
+    # print(parity)
+
+    temp=""
+
+    for i in range(size_of_file) :
+        temp = temp + 'X'
+
+    t = bytes(temp, encoding='latin-1')
+
+    # print(t)
+
+    if(2*size_of_file> 254) :    #127
+        rsc = RSCodec(254)
+    else :
+        rsc = RSCodec(2*size_of_file)
+
+    temp1 = t + parity
+
+    # print(temp1)
+
+    # Decoding (repairing)
+
+    decoded_msg, decoded_msgecc, errata_pos = rsc.decode(temp1)
+
+    # print(decoded_msg)
+
+    data = str(decoded_msg)
+
+    data = data.removeprefix("bytearray(b'")
+    data = data.removesuffix("')")
+
+    # print(data)
+
+    fli="./media/"+file_name+".txt"
+    f = open(fli, "w")
+    f.write(data)
+
+    print('File is decoded successfully')
+
+    # print(data)
---
+++ encode.py
@@ -0,0 +1,55 @@
+import os
+from reedsolo import RSCodec
+from pathlib import Path
+import array
+
+str1=""
+file_name="input"
+fli="./media/"+file_name+".txt"
+f = open(fli, "r")
+for i in f :
+    str1=str1+(i)
+f.close()
+
+size_of_file=len(str1)
+
+if(2*size_of_file > 254) :    #127
+    rsc = RSCodec(254)
+else :
+    rsc = RSCodec(2*size_of_file)
+
+b_str=bytes(str1, 'utf-8')
+
+# print(b_str)
+
+encoded_msg= rsc.encode(b_str)
+
+# print(encoded_msg)
+
+parity_block = encoded_msg.removeprefix(b_str)
+
+# print(parity_block)
+
+# print(size_of_file)
+os.chdir('./media')
+os.mkdir("user")
+# fli1=file_name+"_original_len.txt"
+fle="./user/"+file_name+"_encode.bin"
+f = open(fle, "wb")
+
+f.write(parity_block)
+
+f.close()
+
+size = str(size_of_file)
+
+fli1="./user/"+file_name+"_original_len.txt"
+# os.mkdir("user")
+fli1=file_name+"_original_len.txt"
+f=open(fli1, "w")
+f.write(size)
+
+print('File is Encoded successfully')
+
+
+
